3eabf78b310face3f7a92f9e58ba4c10098416ed
Adam19981 <luming369852@qq.com>
2023-02-18 17:27:15 +0800

    init

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,30 +1,17 @@
-<!--<template>-->
-<!--	<V3Emoji @click-emoji="appendText" :options-name="optionsName" :recent="true"></V3Emoji>-->
-<!--	<textarea v-model="inputText" style="width: 300px" />-->
-<!--</template>-->
+<template>
+	<textarea id="editor" name="editor"></textarea>
+</template>
 
-<!--<script lang="ts" setup>-->
-<!--import V3Emoji from "vue3-emoji";-->
-<!--import "vue3-emoji/dist/style.css";-->
-<!--import { ref } from "vue";-->
+<script lang="ts" setup>
+import { Jodit } from "jodit";
+import "jodit/build/jodit.min.css";
 
-<!--const inputText = ref("");-->
+import { onMounted } from "vue";
+let editor: Jodit;
+onMounted(() => {
+	editor = Jodit.make("#editor");
+	editor.value = '<div style="color:red">测试</div>';
+});
+</script>
 
-<!--const optionsName = {-->
-<!--	"Smileys & Emotion": "笑脸&表情",-->
-<!--	"Food & Drink": "食物&饮料",-->
-<!--	"Animals & Nature": "动物&自然",-->
-<!--	"Travel & Places": "旅行&地点",-->
-<!--	"People & Body": "人物&身体",-->
-<!--	Objects: "物品",-->
-<!--	Symbols: "符号",-->
-<!--	Flags: "旗帜",-->
-<!--	Activities: "活动"-->
-<!--};-->
-<!--function appendText(event: any) {-->
-<!--	console.log(event);-->
-<!--	inputText.value = event;-->
-<!--}-->
-<!--</script>-->
-
-<!--<style></style>-->
+<style></style>
